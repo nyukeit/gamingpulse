b3ea819d924827b2e0cd745a4631e9dbd27c287d
Nyukeit <nyukeit@outlook.com>
2023-11-28 16:08:54 +0100

    [ADD] Left Nav Links + Right SB Demo

--- src/components/PrimaryCard/PrimaryCard.jsx
+++ src/components/PrimaryCard/PrimaryCard.jsx
@@ -1,6 +1,10 @@
+import './PrimaryCard.css'
+
 const PrimaryCard = () => {
   return (
-    <div>PrimaryCard</div>
+    <div className="primary-card">
+      <img src="https://wehco.media.clients.ellingtoncms.com/imports/adg/photos/204035039_0410jason001_t800.jpg?90232451fbcadccc64a17de7521d859a8f88077d" className="card-img-top" alt="primary-card" />
+    </div>
   )
 }
 
--- src/components/RightSidebar/RightSidebarCard/RightSidebarCard.jsx
+++ src/components/RightSidebar/RightSidebarCard/RightSidebarCard.jsx
@@ -1,6 +1,17 @@
+import './RightSidebarCard.css'
+
 const RightSidebarCard = () => {
   return (
-    <div>RightSidebarCard</div>
+    <div className="card">
+      <div className="card-img">
+        <img src="https://cdn.tutsplus.com/cdn-cgi/image/width=600/psd/uploads/legacy/psdtutsarticles/linkb_60vgamecovers/45.jpg" alt="card-img" className="card-thumbnail" />
+      </div>
+      <div className="card-meta">
+        <h4>Game Title</h4>
+        <p>Genre</p>
+        <span><i></i>Rating</span>
+      </div>
+    </div>
   )
 }
 
