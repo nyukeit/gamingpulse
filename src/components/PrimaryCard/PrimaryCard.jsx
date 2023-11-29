cce86705a1888365548f636564b11177c06f9cf1
mounir-mansi <mansimounir1@gmail.com>
2023-11-29 13:42:00 +0100

    main_card_qui_defile_axios

--- src/components/PrimaryCard/PrimaryCard.jsx
+++ src/components/PrimaryCard/PrimaryCard.jsx
@@ -1,36 +1,6 @@
 import './PrimaryCard.css';
-import { useState, useEffect } from 'react';
-import { fetchGameData } from './fetch';
 
-const PrimaryCard = () => {
-  const [games, setGames] = useState([]);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchGameData();
-      setGames(data);
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Vérifie s'il y a des jeux à afficher
-      if (games.length > 0) {
-        setCurrentCardIndex((prevIndex) => {
-          // Incrémente l'index de la carte actuellement affichée
-          // Si c'est le dernier jeu, retourne au début (index 0)
-          return prevIndex === games.length - 1 ? 0 : prevIndex + 1;
-        });
-      }
-    }, 10000); // Change toutes les 3 secondes (ajuste la durée selon tes préférences)
-
-    // Nettoie l'intervalle lors du démontage du composant
-    return () => clearInterval(interval);
-  }, [games]); // Exécute l'effet à chaque changement de jeux
-
+const PrimaryCard = ({ games, currentCardIndex }) => {
   return (
     <div className="primary-card">
       {games.length > 0 && (
